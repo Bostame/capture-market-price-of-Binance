0677335a137e93eca84aed4476c3738d14bc32b5
Bostame Md Bayazid <bbsony.cse@gmail.com>
2019-03-04 19:47:23 +0100

    error correction

--- market_price.js
+++ market_price.js
@@ -1,43 +1,53 @@
 /*-------------------Instantiate an exchange class from ccxt---------------------------------*/
 const ccxt = require ('ccxt')
+log  = require ('ololog').configure ({ locate: false })
 /*----------------------------------Handling uncaughtException--------------------------------*/
 
-process.on ('uncaughtException',  e => { log.error (e); process.exit (1) })
-process.on ('unhandledRejection', e => { log.error (e); process.exit (1) })
+process.on ('uncaughtException',  e => { log.bright.red.error (e); process.exit (1) })
+process.on ('unhandledRejection', e => { log.bright.red.error (e); process.exit (1) })
 
 /*--------------------------------instantiated Symbols----------------------------------------*/
 const symbols = ['BNB/BTC']
 
 /*-------------------instantiated Exchange(Binance)------------------------------------------*/
-const exchange = 'binance'
+const exchangeId = 'binance'
+    , exchangeClass = ccxt[exchangeId]
+    , exchange = new exchangeClass ({
+        'apiKey': 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A',
+        'secret': '	NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j',
+        'timeout': 30000,
+        'enableRateLimit': true,
+    })
 /*------------------------Get price of specific symbols--------------------------------------*/
 async function market_price() {
     try {
       if (exchange.has['fetchTickers']) {
-          console.log(await (exchange.fetchTickers (symbols['price'])))
+          console.log(await (exchange.fetchTickers (symbols)))
       }
       else {
-            console.log(exchange.id + ' does not have ' + symbols)
+            colsole.log(exchange.id + ' does not have ' + symbols)
         }
 
     } catch (e) {
+
         if (e instanceof ccxt.DDoSProtection) {
-            console.log(exchange.id, '[DDoS Protection]')
+            log.bright.yellow (exchange.id, '[DDoS Protection] ' + e.message)
         } else if (e instanceof ccxt.RequestTimeout) {
-            console.log(exchange.id, '[Request Timeout]')
+            log.bright.yellow (exchange.id, '[Request Timeout] ' + e.message)
         } else if (e instanceof ccxt.AuthenticationError) {
-            console.log(exchange.id, '[Authentication Error]')
+            log.bright.yellow (exchange.id, '[Authentication Error] ' + e.message)
         } else if (e instanceof ccxt.ExchangeNotAvailable) {
-            console.log(exchange.id, '[Exchange Not Available]')
+            log.bright.yellow (exchange.id, '[Exchange Not Available] ' + e.message)
         } else if (e instanceof ccxt.ExchangeError) {
-            console.log (exchange.id, '[Exchange Error]')
+            log.bright.yellow (exchange.id, '[Exchange Error] ' + e.message)
         } else if (e instanceof ccxt.NetworkError) {
-            console.log(exchange.id, '[Network Error]')
+            log.bright.yellow (exchange.id, '[Network Error] ' + e.message)
         } else {
             throw e
         }
     }
 }
+
 /*-------------------------interval--------------------------------------------------------*/
 function intervalFunc() {
 market_price()
--- price_list.js
+++ price_list.js
@@ -1,9 +1,5 @@
 /*-------------------instantiated Exchange(Binance)------------------------------------------*/
-const binance = require('node-binance-api')().options({
-  APIKEY: 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A',
-  APISECRET: 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j',
-  useServerTime: true // If you get timestamp errors, synchronize to server time at startup
-});
+const binance = require('node-binance-api')
 
 function intervalFunc() {
   binance.prices((error, ticker) => {
